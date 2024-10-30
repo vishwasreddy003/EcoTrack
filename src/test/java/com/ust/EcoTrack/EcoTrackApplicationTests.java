841fdd37d75c9da13fde6f4275e7b8b171d31c10
vishwasreddy003 <vishwassai123@gmail.com>
2024-10-30 15:58:57 +0530

    Initial Commit

---
+++ src/test/java/com/ust/EcoTrack/EcoTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.EcoTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

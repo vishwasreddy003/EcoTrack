44dbb2c1c88327316957f9082ccb86b22df62495
vishwasreddy003 <vishwassai123@gmail.com>
2024-11-01 10:17:54 +0530

    Updated User

--- src/main/java/com/ust/EcoTrack/Repository/User.java
+++ src/main/java/com/ust/EcoTrack/Repository/User.java
@@ -1,7 +1,6 @@
 package com.ust.EcoTrack.Repository;
 
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Users extends JpaRepository<Users,Integer> {
+public interface User extends JpaRepository<org.apache.catalina.User,Integer> {
 }
